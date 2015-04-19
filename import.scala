70bccc5b76c89b967c364b049b81705ad00407db
Michael M. Wasser <untoldone@users.noreply.github.com>
2015-04-18 22:57:12 -0700

    fetch repo info

--- import.scala
+++ import.scala
@@ -1,3 +1,5 @@
+import scalaj.http._
+
 object HelloWorld {
   def main(args: Array[String]) {
     var parts = args(0).split("/")
@@ -5,5 +7,7 @@ object HelloWorld {
     var repo = parts(1)
     println(user)
     println(repo)
+    val response: HttpResponse[String] = Http("https://api.github.com/repos/" + user + "/" + repo).asString
+    println(response.body)
   }
 }
